c566588e1a55f9cf5e266363a4fce2b966b78b2e
Umed <yusupovjr19@gmail.com>
2022-09-14 17:56:17 +0200

    made some changes in First Java Class

--- src/main/java/FirstJavaClass.java
+++ src/main/java/FirstJavaClass.java
@@ -1,5 +1,7 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
+        System.out.println("Adding a new code");
+        System.out.println("this is another code");
     }
 }
